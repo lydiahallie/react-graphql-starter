ee109b9c355bdd9b1544505875495dc72529561d
Lydia Hallie <lydiajuliettehallie@gmail.com>
2019-01-07 19:42:12 +0100

    Add fake data

---
+++ server/fake_data.js
@@ -0,0 +1,32 @@
+export const messages = [
+  {
+    title: "Awesome message",
+    body: "Such message. Much wow.",
+    author: "Lydia Hallie"
+  },
+  {
+    title: "The weather is great",
+    body: "It's so warm and sunny! I love summer",
+    author: "Lydia Hallie"
+  },
+  {
+    title: "Dogs are amazing",
+    body: "Like honestly why can't there be more dogs?",
+    author: "Lydia Hallie"
+  },
+  {
+    title: "I want to eat cookies",
+    body: "Especially snickerdoodle cookies.",
+    author: "Lydia Hallie"
+  },
+  {
+    title: "I need more coffee",
+    body: "Or maybe not I've had too many",
+    author: "Lydia Hallie"
+  },
+  {
+    title: "Awesome message",
+    body: "This is so amazing, like wow",
+    author: "Lydia Hallie"
+  }
+];
